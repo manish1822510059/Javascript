dee917721322aa9703bd9fefba3e22c427cb94ce
Manish kumar <manish5854758@gmail.com>
2022-04-02 21:07:29 +0530

    Sorting number and String

--- sort_method.js
+++ sort_method.js
@@ -1,1 +1,16 @@
 // node sort_method.js
+// const number = [4,22,21,17,38,9,10]  /
+// number.sort();  // sort number ko string ma change kar ka sort karta ha.
+// console.log("Sortd => " + number);
+
+const number = [4,22,21,17,38,9,10] 
+number.sort((a,b)=> a-b); 
+console.log(number)
+
+// 1200 ,410
+// a-b --------> 790 
+// a-b --------> positive (greater than 0 ) --------> b,a
+//410 ,1200
+
+//a-b --------> negative --> a,b 
+//5,9 --------> -4
